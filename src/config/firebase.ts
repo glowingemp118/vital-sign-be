173e151e0a29f6bee3bb863368d5bc90838d0ed1
GlowingsoftJDev <147134396+glowingemp118@users.noreply.github.com>
2026-02-23 16:22:27 +0500

    fix firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,11 @@
 import * as admin from 'firebase-admin';
-import * as path from 'path';
-
-const serviceAccount = require(
-  path.join(
-    process.cwd(),
-    'src/config/vital-signs-b20b6-firebase-adminsdk-fbsvc.json',
-  ),
-);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 export default admin;
